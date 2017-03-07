59cb2288baafd4452c13bc8f9c23b9327e5ea8cf
Steven Brunwasser <sbrunwasser@gmail.com>
2017-03-07 12:04:34 -0800

    Initial Commit

---
+++ Reggie/Reggie.h
@@ -0,0 +1,19 @@
+//
+//  Reggie.h
+//  Reggie
+//
+//  Created by Steve Brunwasser on 3/7/17.
+//  Copyright © 2017 Steve Brunwasser. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Reggie.
+FOUNDATION_EXPORT double ReggieVersionNumber;
+
+//! Project version string for Reggie.
+FOUNDATION_EXPORT const unsigned char ReggieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reggie/PublicHeader.h>
+
+
